500a866a67d901566b55aca94af643008dbef9f0
Aman Bisht <119600742+Aman95495@users.noreply.github.com>
2023-05-19 19:29:34 +0530

    Add files via upload

---
+++ dynamic_page.js
@@ -0,0 +1,11 @@
+
+
+function showMenu(){
+	var navlinks = document.getElementById("navlinks");
+	navlinks.style.right = "0";
+}
+
+function hideMenu(){
+	var navlinks = document.getElementById("navlinks");
+	navlinks.style.right = "-200px";
+}
